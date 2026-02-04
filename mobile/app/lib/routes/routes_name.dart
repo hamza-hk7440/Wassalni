21caca603b82ac74e1f24c390321330b56280bd6
hamza <khlaiefhamza@gmail.com>
2026-02-04 09:49:05 +0100

    flutter structure

---
+++ mobile/app/lib/routes/routes_name.dart
@@ -0,0 +1,8 @@
+class Routes {
+  // AUTH ROUTES
+  static const String splash = '/';
+  static const String roleSelection = '/role-selection';
+  static const String login = '/login';
+  static const String controllerLogin = '/controller-login';
+  static const String signup = '/signup';
+}
