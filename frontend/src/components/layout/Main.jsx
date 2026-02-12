fe0042bb25596451b6a0a85b99689f1f8f026205
rayen raddaoui <rayenraddaoui59@gmail.com>
2026-02-13 00:25:58 +0100

    modification tailwind et mainlyout

--- frontend/src/components/layout/Main.jsx
+++ frontend/src/components/layout/Main.jsx
@@ -1,31 +1,69 @@
+
+import React, { useState, useEffect } from 'react';
 function MainLayout() {
+
+ {/* const [stats, setStats] = useState({ paiements: 0, reservations: 0, annulations: 0, metroEnService: 0 ,busEnservice: 0});
+  const [loading, setLoading] = useState(true);
+
+  
+  useEffect(() => {
+    fetch('http://votre-api.com/stats') 
+      .then(response => response.json())
+      .then(data => {
+        setStats(data);
+        setLoading(false);
+      })
+      .catch(error => console.error("Erreur backend:", error));
+  }, []);
+  if (loading) return <div className="p-10 text-center text-blue-600 font-bold">Chargement...</div>;*/}
+  const stats = {
+    paiements: 75,
+    reservations: 20,
+    annulations: 5,
+    metroEnService: 10,
+    busEnservice: 15
+  };
   return (
-   
-    <div className="min-h-screen pt-24 p-10 flex flex-col items-center gap-12 bg-slate-50">
-       <div>
+    <>
+    
+    <div className=" min-h-screen pt-24 p-10 flex flex-col items-center gap-12 bg-slate-50">
+
+
+      <div className="flex gap-8 w-full justify-center"> {/*hadha div li fih les stats w les graphiques ya3ni lkbir*/}
+       <div className="bg-white p-6 rounded-xl shadow-sm border-l-4 border-green-500 w-64 text-center">{/* hadha div ta3 kol karou*/}
+          <span className="text-3xl font-bold block">{stats.metroEnService}</span>
+            <span className="text-gray-500">metro en service</span>
        </div>
-      
+      <div className="bg-white p-6 rounded-xl shadow-sm border-l-4 border-green-500 w-64 text-center">
+          <span className="text-3xl font-bold block">{stats.busEnservice}</span>
+          <span className="text-gray-500">bus en service</span>
+        </div>
+      </div>
+       
+       
+
       <div className="flex justify-center gap-15">
       
         <div className="flex flex-col items-center gap-5">
-          <div className="pie" style={{ "--p": 75, "--c": "#3b82f6" }}> 75% </div>
+          <div className="pie" style={{ "--p": stats.paiements, "--c": "#3b82f6" }}> {stats.paiements}% </div>
           <span className="font-medium text-gray-600">Paiements</span>
         </div>
 
   
         <div className="flex flex-col items-center gap-5">
-          <div className="pie" style={{ "--p": 20, "--c": "#10b981" }}> 20% </div>
+          <div className="pie" style={{ "--p": stats.reservations, "--c": "#10b981" }}> {stats.reservations}% </div>
           <span className="font-medium text-gray-600">Réservations</span>
         </div>
 
       
         <div className="flex flex-col items-center gap-5">
-          <div className="pie" style={{ "--p": 5, "--c": "#ef4444" }}> 5% </div>
+          <div className="pie" style={{ "--p": stats.annulations, "--c": "#ef4444" }}> {stats.annulations}% </div>
           <span className="font-medium text-gray-600">Annulations</span>
         </div>
       </div>
 
     </div>
+    </>
   );
 }
 
--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -3,76 +3,86 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-<div className='fixed top-0 left-0 right-0 h-20 bg-blue-600 z-30 
-flex items-center px-4 shadow-md'>      
-<h1 className="text-white text-2xl font-bold ml-25">
-        Mon Application
-    </h1>
-    </div>
-    {isOpen === false && (  
-      <button className="fixed top-4 left-4 z-50 p-2 bg-blue-600 text-white rounded-md 
-      shadow-lg hover:bg-blue-700 transition-colors" onClick={() => setIsOpen(true)}>☰ menu</button>
-    )}
-    {isOpen === true && (  
-      <button className="fixed top-4 left-4 z-50 p-2 bg-red-600 text-white rounded-md shadow-lg 
-      hover:bg-red-700 transition-colors" onClick={() => setIsOpen(false)}>✖️ close</button>
-    )}   
-  <nav className={`fixed top-0 left-0 h-full w-54 bg-white shadow-2xl z-40 transform transition-transform duration-300 ease-in-out ${
+    <div className="fixed inset-0  z-30 "
+          onClick={() => setIsOpen(false)} style={{ backgroundColor: isOpen ? 'rgba(0, 0, 0, 0.5)' : 'transparent' }}></div>
+      {/* hadhi lbar lfo9ania  */}
+      <div className='fixed top-0 left-0 right-0 h-16 bg-white border-b border-slate-100 z-30 
+      flex items-center px-8 shadow-sm justify-between'>      
+        <h1 className="text-blue-600 text-xl font-black tracking-tight ml-16">
+          TRANSPORT<span className="text-slate-400 font-light">PRO</span>
+        </h1>
+        <div className="flex items-center gap-4 text-slate-500 text-sm font-medium">
+            Admin Panel
+        </div>
+      </div>
+
+      {/* bottonati ili y7ilo w ysakrou lbar 3la jnab */}
+      {!isOpen ? (  
+        <button className="fixed top-3.5 left-4 z-50 p-2 bg-slate-50 text-slate-600 rounded-lg 
+        hover:bg-blue-50 hover:text-blue-600 transition-all border border-slate-200" 
+        onClick={() => setIsOpen(true)}>
+          <span className="">☰</span>
+        </button>
+      ) : (  
+        <button className="fixed top-3.5 left-4 z-50 p-2 bg-red-50 text-red-600 rounded-lg 
+        hover:bg-red-100 transition-all border border-red-100" 
+        onClick={() => setIsOpen(false)}>
+          <span className="text-xl">✕</span>
+        </button>
+      )}   
+
+      {/* lbar 3la jnab */}
+      <nav className={`fixed inset-y-0 left-0 w-72 bg-white shadow-2xl z-40 transform transition-transform duration-500  ${
         isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
-        <div className="p-6 pt-20">
-          <ul className="flex flex-col space-y-10">
+        
+        <div className="flex flex-col h-full p-6 pt-24">
+          <p className="text-xs font-bold text-slate-400 uppercase tracking-[0.2em] mb-6">Menu Principal</p>
+          
+          <ul className="flex flex-col space-y-2 flex-grow">
             <li>
-              <Link to="/client" onClick={() => setIsOpen(false)} className="hover:text-blue-200 
-                transition duration-300 font-semibold
-                 hover:underline">
-                👤 Client
+              <Link to="/client" onClick={() => setIsOpen(false)} 
+                className="flex items-center gap-3 px-4 py-3 text-slate-600 hover:bg-blue-50 hover:text-blue-600 rounded-xl transition-all duration-200 font-semibold group">
+                <span className="group-hover:scale-120 transition-transform">👤</span> Client
               </Link>
             </li>
             <li>
-              <Link 
-              onClick={() => setIsOpen(false)}
-                to="/controleur" 
-                className="hover:text-blue-200 transition duration-300 font-semibold hover:underline"
-              >
-                👨‍💼 Contrôleur
+              <Link to="/controleur" onClick={() => setIsOpen(false)} 
+                className="flex items-center gap-3 px-4 py-3 text-slate-600 hover:bg-blue-50 hover:text-blue-600 rounded-xl transition-all duration-200 font-semibold group">
+                <span className="group-hover:scale-120 transition-transform">👨‍💼</span> Contrôleur
               </Link>
             </li>
             <li>
-              <Link 
-              onClick={() => setIsOpen(false)}
-                to="/transport" 
-                className="hover:text-blue-200 transition duration-300 font-semibold hover:underline"
-              >
-                🚌 Transport
+              <Link to="/NvAdmin" onClick={() => setIsOpen(false)} 
+                className="flex items-center gap-3 px-4 py-3 text-slate-600 hover:bg-blue-50 hover:text-blue-600 rounded-xl transition-all duration-200 font-semibold group">
+                <span className="group-hover:scale-120 transition-transform">⚙️</span> Nouveau Admin
               </Link>
             </li>
             <li>
-              <Link 
-                to="/ticket" 
-                onClick={() => setIsOpen(false)}
-                className="hover:text-blue-200 transition duration-300 font-semibold hover:underline "
-              >
-                🎫 Ticket
+              <Link to="/transport" onClick={() => setIsOpen(false)} 
+                className="flex items-center gap-3 px-4 py-3 text-slate-600 hover:bg-blue-50 hover:text-blue-600 rounded-xl transition-all duration-200 font-semibold group">
+                <span className="group-hover:scale-120 transition-transform">🚌</span> Transport
+              </Link>
+            </li>
+            <li>
+              <Link to="/ticket" onClick={() => setIsOpen(false)} 
+                className="flex items-center gap-3 px-4 py-3 text-slate-600 hover:bg-blue-50 hover:text-blue-600 rounded-xl transition-all duration-200 font-semibold group">
+                <span className="group-hover:scale-120 transition-transform">🎫</span> Ticket
               </Link>
             </li>
           </ul>
 
-          <ul>
-            <li className="mt-auto border-t pt-6">
-              <Link 
-                to="/login" 
-                onClick={() => setIsOpen(false)}
-                className="flex items-center text-red-600 hover:text-red-800 transition duration-300 
-                font-bold hover:underline space-y-40">
-               🚪 deconnexion
-              </Link>
-            </li>
-          </ul>
+          
+          <div className="mt-auto pt-6 border-t border-slate-100">
+            <Link to="/login" onClick={() => setIsOpen(false)} 
+              className="flex items-center gap-3 px-4 py-3 text-red-500 hover:bg-red-50 rounded-xl transition-all duration-200 font-bold group">
+              <span className="group-hover:rotate-12 transition-transform">🚪</span> Deconnexion
+            </Link>
           </div>
-    </nav>
-    
+        </div>
+      </nav>
     </>
   );
 }
