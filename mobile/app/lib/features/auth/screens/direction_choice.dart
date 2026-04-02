6f60bda7982ca97025229242eaa407be43f1df6e
khadhraoui Ranim <ranimkhadhraoui1@gmail.com>
2026-04-02 03:30:31 +0100

    direction choice screen done

---
+++ mobile/app/lib/features/auth/screens/direction_choice.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:app/core/theme/colors_R.dart';
+
+class DirectionChoice extends StatelessWidget {
+  const DirectionChoice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.colorL,
+      body: SafeArea(
+      child: Align (alignment: AlignmentGeometry.center,
+      child: SingleChildScrollView(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [ 
+                Image.asset(
+                  "assets/trainnn.png", 
+                  height: 100,
+                  colorBlendMode: BlendMode.srcIn,
+                ),
+                const SizedBox(height: 25),
+                Text(
+                  "Which direction?",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.poppins(
+                    fontSize: 27,
+                    fontWeight: FontWeight.w900,
+                    color: AppColors.colorD,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  "Please select your direction to continue",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    color: Colors.grey[600],
+                  ),
+                ),
+                const SizedBox(height: 50),
+
+                _buildDirectionCard(
+                  context,
+                  title: "Monastir    ➔    Mahdia", // Utilisation d'une flèche plus propre
+                  onTap: () {
+                    print("Vers Mahdia");
+                  },
+                ),
+
+                const SizedBox(height: 20),
+
+                _buildDirectionCard(
+                  context,
+                  title: "Mahdia    ➔    Monastir",
+                  onTap: () {
+                    print("Vers Monastir");
+                  },
+                ),
+                const SizedBox(height: 30),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDirectionCard(
+    BuildContext context, {
+    required String title,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(25),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: AppColors.colorA.withOpacity(0.1),
+              blurRadius: 20,
+              offset: const Offset(0, 10),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Expanded( 
+              child: Text(
+                title,
+                style: GoogleFonts.poppins(
+                  fontSize: 20, 
+                  fontWeight: FontWeight.w600,
+                  color: AppColors.colorD,
+                ),
+              ),
+            ),
+            Icon(Icons.arrow_forward_ios, color: AppColors.colorA, size: 18),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- mobile/app/lib/main.dart
+++ mobile/app/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:app/features/auth/screens/login_page.dart';
 import 'package:app/features/auth/screens/home_test.dart';
 import 'package:app/features/auth/screens/role_choice_screen.dart';
 import 'package:app/features/auth/screens/controller_login.dart';
+import 'package:app/features/auth/screens/direction_choice.dart';
 import 'package:app/features/auth/auth_controller.dart'; // Import your controller
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Wasalni',
       // The app starts here, and since AuthController is already "put", it won't crash
-      home: const SplashScreen(),
+      home: const DirectionChoice(),
       getPages: [
         GetPage(name: '/rolechoice', page: () => const RoleChoicePage()),
         GetPage(name: '/login', page: () => const LoginScreen()),
